f719d452e998225ba8562b139181b54a8ec3111f
vs2k5 <63846930+Smei2005@users.noreply.github.com>
2024-08-24 23:55:25 +0300

    Update scripts.js

--- confirms/scripts.js
+++ confirms/scripts.js
@@ -1,32 +1,26 @@
 document.getElementById("button_sleep").onclick = function() {
-    fetch("http://192.168.0.104:8000/suspend", {
+    fetch("http://YOUR IP OR DOMAIN:8000/suspend", {
         method: 'GET',
         mode: 'no-cors'
     }).then(() => {
-        window.location.href = 'https://golian.vercel.app';
-    }).catch(() => {
-        window.location.href = 'https://golian.vercel.app';
-    });
+        window.location.href = '/index.html';
+    })
 }
 
 document.getElementById("button_restart").onclick = function() {
-    fetch("http://192.168.0.104:8000/restart", {
+    fetch("http://YOUR IP OR DOMAIN:8000/restart", {
         method: 'GET',
         mode: 'no-cors'
     }).then(() => {
-        window.location.href = 'https://golian.vercel.app';
-    }).catch(() => {
-        window.location.href = 'https://golian.vercel.app';
-    });
+        window.location.href = '/index.html';
+    })
 }
 
 document.getElementById("button_off").onclick = function() {
-    fetch("http://192.168.0.104:8000/turnOFF", {
+    fetch("http://YOUR IP OR DOMAIN:8000/turnOFF", {
         method: 'GET',
         mode: 'no-cors'
     }).then(() => {
-        window.location.href = 'https://golian.vercel.app';
-    }).catch(() => {
-        window.location.href = 'https://golian.vercel.app';
-    });
+        window.location.href = '/index.html';
+    })
 }

bae8d1efa3699cf6f3a9fca2b6e14fa03cdd6294
vs2k5 <63846930+Smei2005@users.noreply.github.com>
2024-08-24 19:41:11 +0300

    optimize routing

--- confirms/scripts.js
+++ confirms/scripts.js
@@ -1,11 +1,11 @@
 document.getElementById("button_sleep").onclick = function() {
-    fetch("http://192.168.0.104:8000/suspend")
+    fetch("http://192.168.0.104:8000/suspend").then()
 }
 
 document.getElementById("button_restart").onclick = function() {
-    fetch("http://192.168.0.104:8000/restart")
+    fetch("http://192.168.0.104:8000/restart").then()
 }
 
 document.getElementById("button_off").onclick = function() {
-    fetch("http://192.168.0.104:8000/turnOFF")
+    fetch("http://192.168.0.104:8000/turnOFF").then()
 }

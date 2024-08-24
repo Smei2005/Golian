08886fb3de4a7770571162191bc6cece7d2c9a74
vs2k5 <63846930+Smei2005@users.noreply.github.com>
2024-08-24 20:23:56 +0300

    fix routing

--- confirms/scripts.js
+++ confirms/scripts.js
@@ -1,11 +1,32 @@
 document.getElementById("button_sleep").onclick = function() {
-    fetch("http://192.168.0.104:8000/suspend")
+    fetch("http://192.168.0.104:8000/suspend", {
+        method: 'GET',
+        mode: 'no-cors'
+    }).then(() => {
+        window.location.href = 'https://golian.vercel.app';
+    }).catch(() => {
+        window.location.href = 'https://golian.vercel.app';
+    });
 }
 
 document.getElementById("button_restart").onclick = function() {
-    fetch("http://192.168.0.104:8000/restart")
+    fetch("http://192.168.0.104:8000/restart", {
+        method: 'GET',
+        mode: 'no-cors'
+    }).then(() => {
+        window.location.href = 'https://golian.vercel.app';
+    }).catch(() => {
+        window.location.href = 'https://golian.vercel.app';
+    });
 }
 
 document.getElementById("button_off").onclick = function() {
-    fetch("http://192.168.0.104:8000/turnOFF")
+    fetch("http://192.168.0.104:8000/turnOFF", {
+        method: 'GET',
+        mode: 'no-cors'
+    }).then(() => {
+        window.location.href = 'https://golian.vercel.app';
+    }).catch(() => {
+        window.location.href = 'https://golian.vercel.app';
+    });
 }
